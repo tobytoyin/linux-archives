3bfc5b2b8284c40138f63dfc1500e33603c18419
Toby <40096033+tobytoyin@users.noreply.github.com>
2023-11-13 21:11:45 +0000

    remove random since no storage account is needed

--- terraform/azure-vm/main.tf
+++ terraform/azure-vm/main.tf
@@ -69,16 +69,6 @@ resource "azurerm_network_interface_security_group_association" "example" {
   network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
 }
 
-# Generate random text for a unique storage account name
-resource "random_id" "random_id" {
-  keepers = {
-    # Generate a new ID only when a new resource group is defined
-    resource_group = azurerm_resource_group.rg.name
-  }
-
-  byte_length = 8
-}
-
 # Create virtual machine
 resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
   name                  = "myVM"
